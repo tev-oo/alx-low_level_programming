b576a758884cec2aa00fb6c1ce889d2e3054d07e
tev-oo <tevinajode@gmail.com>
2023-03-16 12:54:05 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A hat prints the size of various types on the computer it is compiled and run on
+ * main - A that prints the size of various types
  * Return: 0
  */
 int main(void)
