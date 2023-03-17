ee344a0b5700e731564b8a3063e324daeb861719
tev-oo <tevinajode@gmail.com>
2023-03-17 13:44:17 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,12 @@
+int main(void)
+{
+char alp[52] = "abcdefghijkmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
