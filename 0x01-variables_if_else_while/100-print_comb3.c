e0f9e14bbc4395764f8dde61591bf9e9c3b01719
tev-oo <tevinajode@gmail.com>
2023-03-18 13:15:16 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Outputs possible comination of two digits
+ * Return:0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 48; a <= 56; a++)
+	{
+		for (b = 49; m <= 57; b++)
+		{
+			if (b > a)
+			{
+				putchar(a);
+				putchar(b);
+				if (a != 56 || b != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
