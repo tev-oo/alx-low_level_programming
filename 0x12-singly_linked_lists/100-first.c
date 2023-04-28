598573ed6099cdec34511b499f5666b6e190a401
tev-oo <tevinajode@gmail.com>
2023-04-28 11:04:04 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void hare(void)__attribute__ ();
+
+/**
+ * hare - prints a sentence
+ * function is executed
+ */
+void hare(void)
+{
+printf("You're beat! and yet, you must allow,""\n""I bore my house upon my back!""\n");
+}
