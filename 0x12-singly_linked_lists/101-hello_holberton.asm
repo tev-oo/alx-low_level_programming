a8685fc47e0e0106e4e6dd29e1649c2e789f6fcc
tev-oo <tevinajode@gmail.com>
2023-04-28 11:37:18 -0700

    101-part

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,21 @@
+extern	printf
+
+        section .data
+msg:	db "Hello, Holberton", 0
+fmt:    db "%s", 10, 0
+
+        section .text
+
+        global main
+main:
+        push    rp
+	
+	mov	rdi,fmt
+	mov	rs,msg
+	mov	rax,0
+        call    printf
+
+	pop	rbp
+
+	mov	rax,0
+	ret
